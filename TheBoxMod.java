3928525e5cac654a87ab6bdc44ca91ae92121859
AlexTheSpy <63261062+AlexTheSpy@users.noreply.github.com>
2020-05-07 22:51:11 -0400

    Add files via upload

---
+++ TheBoxMod.java
@@ -0,0 +1,37 @@
+package com.TheBoxCrew.Box;
+
+
+import com.TheBoxCrew.Box.util.RegistryHandler;
+import net.minecraftforge.common.MinecraftForge;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("boxmod")
+public class TheBoxMod
+{
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "boxmod";
+    public TheBoxMod() {
+
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        RegistryHandler.init();
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {  }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {  }
+}
